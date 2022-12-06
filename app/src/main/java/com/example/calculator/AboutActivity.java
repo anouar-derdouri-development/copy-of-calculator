8f7dfa4e38c753bf137a5c3b0b1ef101e43302c2
Anouar DERDOURI <anouar.derdouri.developement@gmail.com>
2022-12-06 21:42:09 +0100

    About Activity: Return Button developed

--- app/src/main/java/com/example/calculator/AboutActivity.java
+++ app/src/main/java/com/example/calculator/AboutActivity.java
@@ -3,8 +3,11 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+    Button btn_return;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,14 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         setTitle("About Calculator App");
+
+        btn_return = findViewById(R.id.btn_return);
+
+        btn_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
